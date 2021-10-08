0e9def18289b5dd9af5355d974706c448467015d
Martyna Michalska <m44rti28@gmail.com>
2021-10-08 20:52:35 +0200

    removed storyboards

--- LearniOS/LoginViewController.swift
+++ LearniOS/LoginViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .cyan
         // Do any additional setup after loading the view.
     }
-
-
 }
 
