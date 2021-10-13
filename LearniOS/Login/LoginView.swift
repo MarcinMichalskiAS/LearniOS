dbd4e657a0c95ab741bf739ed11958ba3cc57b71
Martyna Michalska <m44rti28@gmail.com>
2021-10-13 17:36:13 +0200

    Add button and text fields

--- LearniOS/Login/LoginView.swift
+++ LearniOS/Login/LoginView.swift
@@ -1,8 +1,42 @@
 import UIKit
 
 class LoginView: BaseView {
+    let firstNumberTextField = UITextField()
+    let secondNumberTextField = UITextField()
+    let sumButton = UIButton(type: .system)
     override func setupProperties() {
+        [firstNumberTextField, secondNumberTextField].forEach { textField in
+            textField.keyboardType = .numberPad
+            textField.backgroundColor = .blue
+        }
+        sumButton.backgroundColor = .yellow
+        sumButton.setTitle("SUM", for: .normal)
+        sumButton.setTitle("+", for: .highlighted)
+    
         backgroundColor = .gray
     }
     
+    override func setupConstraints() {
+        [firstNumberTextField, secondNumberTextField, sumButton].forEach { view in
+            addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        firstNumberTextField.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        firstNumberTextField.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        firstNumberTextField.topAnchor.constraint(equalTo: topAnchor, constant: 80).isActive = true
+        firstNumberTextField.leftAnchor.constraint(equalTo: leftAnchor, constant: 15).isActive = true
+        
+        secondNumberTextField.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        secondNumberTextField.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        secondNumberTextField.topAnchor.constraint(equalTo: firstNumberTextField.topAnchor).isActive = true
+        secondNumberTextField.leftAnchor.constraint(equalTo: firstNumberTextField.rightAnchor, constant: 30).isActive = true
+        
+        sumButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        sumButton.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        sumButton.topAnchor.constraint(equalTo: firstNumberTextField.bottomAnchor,constant: 16).isActive = true
+        sumButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        
+    }
 }
--- LearniOS/Login/LoginViewController.swift
+++ LearniOS/Login/LoginViewController.swift
@@ -8,28 +8,32 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    var liczba1: Int = 1
-    
+
     override func loadView() {
         view = customView
     }
-    
+        
     let customView = LoginView()
     
     @objc func openRegister() {
         navigationController?.pushViewController(RegisterVievController(), animated: true)
+        customView.backgroundColor = .red
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openRegister))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open register", style: .plain, target: self, action: #selector(openRegister))
-        customView.backgroundColor = .red
-        view.backgroundColor = .yellow
+        customView.sumButton.addTarget(self, action: #selector(sum), for: .touchUpInside)
     }
     
-
+    @objc func sum() {
+        let value1 = customView.firstNumberTextField.text ?? "0"
+        let value2 = customView.secondNumberTextField.text ?? "0"
+        let intValue1 = Int(value1) ?? 0
+        let intValue2 = Int(value2) ?? 0
+        print(intValue1 + intValue2)
+    }
 }
 
 class RegisterVievController: UIViewController {
